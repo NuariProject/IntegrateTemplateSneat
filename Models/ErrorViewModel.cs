144ad6766e6f324fa21d9f037b34dd1cb128eaed
NuariProject <nuari.br@gmail.com>
2022-04-07 10:43:21 +0700

    First Push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TemplateSneat.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
